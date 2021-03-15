9a6c676db7415a0cc93987e261d9162740007bd8
ryandonggunlee <donggun.lee.01@gmail.com>
2021-03-15 16:51:50 -0500

    Integrate Charts and Conditional Rendering

--- my-app/src/MyApp.jsx
+++ my-app/src/MyApp.jsx
@@ -1,18 +1,99 @@
-import React from "react";
-import { Card, Text } from "@ui5/webcomponents-react";
+import React, { useState } from "react";
+import { Card, Text, Icon } from "@ui5/webcomponents-react";
 import { spacing } from "@ui5/webcomponents-react-base";
+import { BarChart, LineChart } from "@ui5/webcomponents-react-charts";
+import "@ui5/webcomponents-icons/dist/line-chart.js";
+import "@ui5/webcomponents-icons/dist/horizontal-bar-chart.js";
 
 export function MyApp() {
-    const handleHeaderClick = () => {
-        alert("Header clicked");
+  const [toggleCharts, setToggleCharts] = useState("lineChart");
+  const [loading, setLoading] = useState(false);
+  const contentTiltle =
+    toggleCharts === "lineChart" ? "Line Chart" : "Bar Chart";
+  const switchToChart =
+    toggleCharts === "lineChart" ? "Bar Chart" : "Line Chart";
+  const handleHeaderClick = () => {
+    if (toggleCharts === "lineChart") {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+        setToggleCharts("barChart");
+      }, 2000);
+    } else {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+        setToggleCharts("lineChart");
+      }, 2000);
     }
-    return ( 
-        <div>
-            <Card heading="Card" style={{width: "300px"}} headerInteractive onHeaderClick={handleHeaderClick}>
-                <Text style={spacing.sapUiContentPadding}>
-                    This is the content area of the Card
-                </Text>
-            </Card>
-        </div>
-    );
+  };
+  const dataset = [
+    {
+      month: "January",
+      data: 65,
+    },
+    {
+      month: "February",
+      data: 59,
+    },
+    {
+      month: "March",
+      data: 80,
+    },
+    {
+      month: "April",
+      data: 81,
+    },
+    {
+      month: "May",
+      data: 56,
+    },
+    {
+      month: "June",
+      data: 55,
+    },
+    {
+      month: "July",
+      data: 40,
+    },
+  ];
+  return (
+    <div>
+      <Card
+        avatar={
+          <Icon
+            name={
+              toggleCharts === "lineChart"
+                ? "line-chart"
+                : "horizontal-bar-chart"
+            }
+          />
+        }
+        heading="Stock Price"
+        style={{ width: "300px" }}
+        headerInteractive
+        onHeaderClick={handleHeaderClick}
+        subheading={`Click here to switch to ${switchToChart}`}
+      >
+        <Text style={spacing.sapUiContentPadding}>
+          This is the content area of the Card
+        </Text>
+        {toggleCharts === "lineChart" ? (
+          <LineChart
+            dimensions={[{ accessor: "month" }]}
+            measures={[{ accessor: "data", label: "stock Price" }]}
+            dataset={dataset}
+            loading={loading}
+          />
+        ) : (
+          <BarChart
+            dimensions={[{ accessor: "month" }]}
+            measures={[{ accessor: "data", label: "Stock Price" }]}
+            dataset={dataset}
+            loading={loading}
+          />
+        )}
+      </Card>
+    </div>
+  );
 }
